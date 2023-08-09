c0f752d783d4dc84f5f125b64ad6e45c48b2ff44
Aliaksandr Yashchuk <alexander@stiversmith.com>
2023-08-09 08:45:00 -0400

    Project created

---
+++ Homework11/Homework11/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework11
+//
+//  Created by Aliaksandr Yashchuk on 8/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
